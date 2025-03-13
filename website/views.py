e65433d3c4367805cade37b02f2c801d9f0a1722
Refqa <bashirrefqa@gmail.com>
2025-03-13 11:51:28 +0100

    Fixed base dropdown

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.utils.timezone import now
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
