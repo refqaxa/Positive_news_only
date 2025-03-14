c1cc926694b9c5e6e1ac193338c162961ec893b8
Refqa <bashirrefqa@gmail.com>
2025-03-14 11:00:34 +0100

    Re-added news sources

--- website/urls.py
+++ website/urls.py
@@ -3,7 +3,7 @@ from .views import (
     home, article_detail, category_articles, search_articles,
     register, user_login, user_logout, user_profile,
     favorite_articles, toggle_favorite, account_settings_view,
-    source_settings_view
+    source_settings_view, all_sources, source_articles
 )
 
 urlpatterns = [
@@ -12,6 +12,8 @@ urlpatterns = [
     path('category/<str:category>/', category_articles, name='category_articles'),  # Categorie artikelen
     path('search/', search_articles, name='search_articles'),  # Zoek artikels
     path('article/<str:article_id>/', article_detail, name='article_detail'),  # Detailpagina
+    path('all_sources/', all_sources, name='all_sources'),  # Nieuwe pagina voor alle bronnen
+    path('source/<str:source>/', source_articles, name='source_articles'), # Artikels bronnen
     
     # user pagina's
     path('register/', register, name='register'),
